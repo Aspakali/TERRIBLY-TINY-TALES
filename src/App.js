61f37e3d50a820ecce192f439c35c6c0bbc294d0
aspakali <aspakali9091@gmail.com>
2023-11-21 00:47:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './componment/FrontPage'
+import FrontPage from './componment/FrontPage';
+
+function App() {
+  return (
+    <div className="App">
+      <FrontPage/>
+    </div>
+  );
+}
+
+export default App;
